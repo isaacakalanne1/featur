502745f142c1dcce2fa173e16f415c8eb0459900
Isaac Akalanne <iakalann@jaguarlandrover.com>
2023-02-24 09:54:02 +0000

    Initial commit

---
+++ Featur/Featur/FeaturApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeaturApp.swift
+//  Featur
+//
+//  Created by iakalann on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FeaturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
